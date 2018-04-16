43f733878cd3d8ecb61719a1b71ef6caf33d4afc
AnabelleLaurent <alaurent@iastate.edu>
2018-04-16 12:25:29 -0500

    intro sent by Mitch

--- agron508shiny.R
+++ agron508shiny.R
@@ -34,7 +34,9 @@ ui <- dashboardPage(
                            status = "info", solidHeader = TRUE, collapsible = TRUE,
                            "This on-line interactive tool is designed by the students of AGRON 508 spring 2018",
                            br(),
-                           "We included interactive graphs based on our assignments"),
+                    "We included interactive graphs based on our assignment goals (1) to measure the wavelength of 
+                    light that can be observed by a human eye. (2) Calculate potential yield using irradiance
+                    measurements derived from a spectroradiometer."),
                 
                 box(title = "Other box",width=12,
                     status = "success", solidHeader = TRUE, collapsible = TRUE,
